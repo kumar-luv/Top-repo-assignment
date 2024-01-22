32c7dd536cbad9cec55e16e00b3935946f6c3e26
kumar-luv <luvchaudhary4566@gmail.com>
2024-01-22 20:27:38 +0530

    Display Top 30 Repositories

--- src/App.js
+++ src/App.js
@@ -1,9 +1,45 @@
-import './App.css';
+import React, { useState, useEffect } from "react";
+import RepoCard from "./components/RepoCard";
 
-function App() {
+const App = () => {
+  const [topRepos, setTopRepos] = useState([]);
+
+  const [page, setPage] = useState(1);
+
+  const currentDate = new Date();
+  currentDate.setDate(currentDate.getDate() - 30);
+  const todayDate = currentDate.toISOString().split("T")[0];
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        `https://api.github.com/search/repositories?q=created:%3E${todayDate}&sort=stars&order=desc&page=${page}`
+      );
+      if (!response.ok) {
+        throw new Error("Response was not ok");
+      }
+      const data = await response.json();
+      setTopRepos(data.items);
+    } catch (error) {
+      console.error("Error ", error);
+    }
+  };
+  useEffect(() => {
+    fetchData();
+  }, [page]);
+
   return (
-    <div>Hello</div>
+    <div className="text-center mx-auto max-w-screen-md">
+      <h1 className="text-2xl font-bold mb-6">
+        Most Starred GitHub Repositories (Last 30 Days)
+      </h1>
+      <ul className="flex flex-col items-center">
+        {topRepos.map((repo) => (
+          <RepoCard key={repo.id} repo={repo} />
+        ))}
+      </ul>
+    </div>
   );
-}
+};
 
 export default App;
---
+++ src/components/RepoCard.jsx
@@ -0,0 +1,35 @@
+import React from "react";
+import { formatDistanceToNow } from "date-fns";
+const RepoCard = ({ repo }) => {
+  return (
+    <div className="flex w-full m-3 p-3 border border-gray-300 rounded-md shadow-md hover:shadow-2xl transition duration-500">
+      <img
+        className="w-52 h-52 rounded-full object-cover"
+        src={repo?.owner?.avatar_url}
+        alt={`${repo?.owner?.login}'s avatar`}
+      />
+      <div className="flex flex-col ml-4">
+        <h2 className="text-left font-bold text-3xl mt-4">{repo?.name}</h2>
+        <p className="text-left font-medium text-2xl mt-2">{repo?.description}</p>
+        <div className="flex mt-8 gap-8">
+          <a
+            href={repo?.owner?.html_url}
+            target="_blank"
+            className="ml-4 bg-yellow-400 text-white px-4 py-2 rounded hover:bg-yellow-500 transition duration-300 hover:cursor-pointer"
+          >
+            Read More
+          </a>
+          <p className="mt-2 text-yellow-500">Stars: {repo?.stargazers_count}</p>
+          <p className="mt-2 text-yellow-500">Issues: {repo?.open_issues_count}</p>
+        </div>
+        <p className="text-left text-xl text-yellow-300 mt-2">
+          Submitted{" "}
+          <span>{formatDistanceToNow(new Date(repo?.created_at))}</span> ago by{" "}
+          <span>{repo?.owner?.login}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default RepoCard;
